b7ae683cb054d27c14e2ef04e51e8d50c8938a3a
Malcolm Nixon <malcolm.nixon@gmail.com>
2019-01-25 20:28:23 -0500

    Added star type.

--- VR Global Game Jam 2019/Assets/Scripts/PlanetData.cs
+++ VR Global Game Jam 2019/Assets/Scripts/PlanetData.cs
@@ -6,22 +6,39 @@ public class PlanetData
     {
         var rand = new Generator(seed);
 
-        // Select star name
+        // Generate location
+        Location = new Vector3(
+            rand.NextFloat() * 30f - 15f,
+            rand.NextFloat() * 30f - 15f,
+            rand.NextFloat() * 30f - 15f);
+
+        // Generate star name
         StarName = NameGenerator.Create(rand.NextInt());
 
-        // Select planet name
-        PlanetName = NameGenerator.Create(rand.NextInt());
+        // Select star type
+        StarType = rand.NextFrom(StarType.Types);
 
-        // Select location name
-        Location = new Vector3(
-            rand.NextFloat() * 30f - 15f, 
-            rand.NextFloat() * 30f - 15f, 
-            rand.NextFloat() * 30f - 15f);
+        // Generate the star properties
+        var starModifier = rand.NextFloat();
+        StarTemperature = StarType.Temperature.Interpolate(starModifier);
+        StarMass = StarType.Mass.Interpolate(starModifier);
+        StarHabitableZone = StarType.Habitable.Interpolate(starModifier);
+
+        // Generate planet name
+        PlanetName = NameGenerator.Create(rand.NextInt());
     }
 
+    public Vector3 Location { get; }
+
     public string StarName { get; }
+
+    public StarType StarType { get; }
+
+    public float StarTemperature { get; }
+
+    public float StarMass { get; }
+
+    public float StarHabitableZone { get; }
     
     public string PlanetName { get; }
-    
-    public Vector3 Location { get; }
 }
---
+++ VR Global Game Jam 2019/Assets/Scripts/StarType.cs
@@ -0,0 +1,32 @@
+﻿public class StarType : IProbability
+{
+    public static readonly StarType AType = new StarType(0.6f, "A", 7500f, 10000f, 1.4f, 2.1f, 3f, 9f);
+    public static readonly StarType FType = new StarType(3f, "F", 6000f, 7500f, 1.04f, 1.4f, 1.8f, 2f);
+    public static readonly StarType GType = new StarType(7.6f, "G", 5200f, 6000f, 0.8f, 1.04f, 0.7f, 1.8f);
+    public static readonly StarType KType = new StarType(12.1f, "K", 3700f, 5200f, 0.45f, 0.8f, 0.35f, 0.7f);
+
+    public static readonly StarType[] Types =
+    {
+        AType,
+        FType,
+        GType,
+        KType,
+    };
+
+    public float Probability { get; }
+    public string Name { get; }
+    public RangeFloat Temperature { get; }
+    public RangeFloat Mass { get; }
+    public RangeFloat Habitable { get; }
+
+    public StarType(float probability, string name, float minTemp, float maxTemp, float minMass, float maxMass,
+        float minHabit, float maxHabit)
+    {
+        Probability = probability;
+        Name = name;
+        Temperature = new RangeFloat(minTemp, maxTemp);
+        Mass = new RangeFloat(minMass, maxMass);
+        Habitable = new RangeFloat(minHabit, maxHabit);
+    }
+}
+
