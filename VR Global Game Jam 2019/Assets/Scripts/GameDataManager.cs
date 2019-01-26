41cb86498b93a1147d6c0f4fbba6da1b21d81442
Malcolm Nixon <malcolm.nixon@gmail.com>
2019-01-25 19:20:22 -0500

    Added GameDataManager

---
+++ VR Global Game Jam 2019/Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour
+{
+    public GameData Game { get; set; }
+
+    public void NewGame()
+    {
+        // Create new game
+        Game = new GameData(1234);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
