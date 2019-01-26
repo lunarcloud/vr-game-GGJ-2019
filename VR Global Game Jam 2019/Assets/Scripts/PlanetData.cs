ad66dfb5da05f9e364af851547526be35b2d5d0c
Malcolm Nixon <malcolm.nixon@gmail.com>
2019-01-25 20:43:01 -0500

    Added significantly more star information including color.

--- VR Global Game Jam 2019/Assets/Scripts/PlanetData.cs
+++ VR Global Game Jam 2019/Assets/Scripts/PlanetData.cs
@@ -21,11 +21,24 @@ public class PlanetData
         // Generate the star properties
         var starModifier = rand.NextFloat();
         StarTemperature = StarType.Temperature.Interpolate(starModifier);
+        StarColor = StarType.BlackBodyColor(StarTemperature);
         StarMass = StarType.Mass.Interpolate(starModifier);
         StarHabitableZone = StarType.Habitable.Interpolate(starModifier);
 
         // Generate planet name
         PlanetName = NameGenerator.Create(rand.NextInt());
+
+        // Generate planet distance
+        PlanetDistance = StarHabitableZone * (0.9f + rand.NextFloat() * 0.2f);
+
+        // Generate planet diameter
+        PlanetDiameter = 7000 + rand.NextFloat() * 9000;
+
+        // Calculate planet temperature
+        PlanetTemperature = 14 + 200 * (StarHabitableZone / PlanetDistance - 1);
+
+        // Pick gravity based on diameter
+        PlanetGravity = Mathf.Pow(PlanetDiameter, 3) / 2e12f;
     }
 
     public Vector3 Location { get; }
@@ -36,9 +49,19 @@ public class PlanetData
 
     public float StarTemperature { get; }
 
+    public Color StarColor { get; }
+
     public float StarMass { get; }
 
     public float StarHabitableZone { get; }
     
     public string PlanetName { get; }
+
+    public float PlanetDistance { get; }
+
+    public float PlanetDiameter { get; }
+
+    public float PlanetTemperature { get; }
+
+    public float PlanetGravity { get; }
 }
--- VR Global Game Jam 2019/Assets/Scripts/StarType.cs
+++ VR Global Game Jam 2019/Assets/Scripts/StarType.cs
@@ -1,4 +1,6 @@
-﻿public class StarType : IProbability
+﻿using UnityEngine;
+
+public class StarType : IProbability
 {
     public static readonly StarType AType = new StarType(0.6f, "A", 7500f, 10000f, 1.4f, 2.1f, 3f, 9f);
     public static readonly StarType FType = new StarType(3f, "F", 6000f, 7500f, 1.04f, 1.4f, 1.8f, 2f);
@@ -27,6 +29,41 @@
         Temperature = new RangeFloat(minTemp, maxTemp);
         Mass = new RangeFloat(minMass, maxMass);
         Habitable = new RangeFloat(minHabit, maxHabit);
+    }
+
+    public static Color BlackBodyColor(float kelvin)
+    {
+        var x = kelvin / 1000.0f;
+        var x2 = x * x;
+        var x3 = x2 * x;
+        var x4 = x3 * x;
+        var x5 = x4 * x;
+
+        // red
+        float red;
+        if (kelvin <= 6600)
+            red = 1f;
+        else
+            red = 0.0002889f * x5 - 0.01258f * x4 + 0.2148f * x3 - 1.776f * x2 + 6.907f * x - 8.723f;
+
+        // green
+        float green;
+        if (kelvin <= 6600)
+            green = -4.593e-05f * x5 + 0.001424f * x4 - 0.01489f * x3 + 0.0498f * x2 + 0.1669f * x - 0.1653f;
+        else
+            green = -1.308e-07f * x5 + 1.745e-05f * x4 - 0.0009116f * x3 + 0.02348f * x2 - 0.3048f * x + 2.159f;
+
+        // blue
+        float blue;
+        if (kelvin <= 2000f)
+            blue = 0f;
+        else if (kelvin < 6600f)
+            blue = 1.764e-05f * x5 + 0.0003575f * x4 - 0.01554f * x3 + 0.1549f * x2 - 0.3682f * x + 0.2386f;
+        else
+            blue = 1f;
+
+        // Return the new color
+        return new Color(red, green, blue);
     }
 }
 
